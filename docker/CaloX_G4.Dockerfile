7a09f8609336496afacfc00e1d431e5f6445947a
Christopher Cowden <chris.cowden@gmail.com>
2025-01-14 19:55:19 -0600

    Building hgcal docker based on calox G4 simulation

--- docker/CaloX_G4.Dockerfile
+++ docker/CaloX_G4.Dockerfile
@@ -1,5 +1,10 @@
 FROM calox
 
+SHELL ["/usr/bin/bash", "-c"]
+RUN /opt/miniconda/bin/conda init && \
+    source ~/.bashrc && \
+    conda install python=3.11
+
 ADD dkr_build_calox_g4.sh /opt/dkr_build_calox_g4.sh
 
 RUN bash /opt/dkr_build_calox_g4.sh 
--- docker/dkr_build_calox_g4.sh
+++ docker/dkr_build_calox_g4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.bashrc
+
 source /opt/geant4/install/share/Geant4/geant4make/geant4make.sh
 echo "Cloning CaloX_G4"
 git clone https://github.com/cowden/CaloX_G4 --branch=dev -- /opt/CaloX_G4
@@ -11,3 +13,11 @@ cd build
 cmake ../
 make
 echo "CaloX_G4 build complete: /opt/CaloX_G4/build/"
+
+echo "Installing CaloX_G4 python utilities"
+echo `which conda`
+conda init
+conda activate base
+conda install -y python=3.11 numpy h5py
+cd /opt/CaloX_G4/python
+python -m pip install .
